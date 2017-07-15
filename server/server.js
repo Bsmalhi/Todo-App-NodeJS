4b8d544cff9dec93d102851cb913a017541898c8
Baljot Malhi <baljotmalhi11@gmail.com>
2017-07-15 12:13:09 -0700

    Added GET /todos route in the TodoAPP

--- server/server.js
+++ server/server.js
@@ -10,15 +10,6 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res)=>{
-    // var todo = new Todo({
-    //     text: req.body.text
-    // });
-
-    // todo.save().then((doc)=>{
-    //     res.send(doc);
-    // },(e)=>{
-    //     res.status(400).send(e);
-    // });
 
     var todo1 = new Todo({
         text: req.body.text,
@@ -31,6 +22,14 @@ app.post('/todos', (req, res)=>{
         res.status(400).send(e);
     });
 
+});
+
+app.get('/todos', (req, res)=>{
+    Todo.find().then((todos)=>{
+        res.send({ todos });
+    },(e)=>{
+        res.status(400).send(e);
+    });
 });
 
     
